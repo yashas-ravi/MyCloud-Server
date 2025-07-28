9528f21a9de20789aaf2ecc88446d4f7a5b63c95
yashas mr <yashasravi03@gmail.com>
2025-07-28 21:08:50 +0530

    service file update

--- src/services/linuxService.cpp
+++ src/services/linuxService.cpp
@@ -5,6 +5,8 @@
 #include <string>
 #include <fstream>
 #include <sys/statvfs.h>
+#include <unistd.h>
+#include <iomanip>
 using namespace std;
 
 //read cpu usage
@@ -31,35 +33,48 @@ float cpuStat(){
 }
 
 //read ram usage
-void ramStat(size_t &used, size_t &total){
+void ramStat(double &used, double &total){
     ifstream stat("/proc/meminfo");
     string line;
     size_t totalMemory=0, avalMemory=0;
 
     while(getline(stat,line)){
-        if(line.find("MemTotal:")){
+        if(line.find("MemTotal:")==0){
             sscanf(line.c_str(),"MemTotal: %zu kB",&totalMemory);
         }
-        if(line.find("MemAvailable")){
+        if(line.find("MemAvailable")==0){
             sscanf(line.c_str(),"MemAvailable: %zu kB",&avalMemory);
         }
     }
     stat.close();
-    total=totalMemory/1024;
-    used=(totalMemory-avalMemory)/1024;
+    total=totalMemory/(1024.0*1024.0);
+    used=(totalMemory-avalMemory)/(1024.0*1024.0);
 }
 
 void storageStat(){
     struct statvfs stat;
-    if(statvfs("/",&stat)!=0){
+    if(statvfs("/home",&stat)!=0){
         cerr<<"Failed to get storage info !!! \n";
+        return;
     }
-
-    unsigned long long total = stat.f_blocks * stat.f_frsize;
-    unsigned long long free = stat.f_bavail * stat.f_frsize;
-    unsigned long long used = total-free;
+    unsigned long long totalbytes = stat.f_blocks * stat.f_frsize;
+    unsigned long long freebytes = stat.f_bfree * stat.f_frsize;
+    unsigned long long usedbytes = totalbytes-freebytes;
+    double total = totalbytes/(1024*1024*1024);
+    double used = usedbytes/(1024*1024*1024);
+    cout<<fixed<<setprecision(2);
+    cout<<"Storage: "<<used<<" / "<<total<<" GB\n";
 }
 
-void main(){
-
+int main(){
+    float cpu = cpuStat();
+    sleep(1);
+    cpu=cpuStat();
+    double ramUsed=0.0, ramTotal=0.0;
+    ramStat(ramUsed,ramTotal);
+    cout<<fixed<<setprecision(2);
+    cout<<"CPU: "<<cpu<<" usage\n";
+    cout<<"RAM: "<<ramUsed<<" / "<<ramTotal<<" GB\n";
+    storageStat();
+    return 0;
 }
